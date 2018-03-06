425eaecd154ee1d74ddebfcd46f73f3012465f24
Matthew Gallagher <mattva01@gmail.com>
2018-03-06 16:12:54 -0500

    Version 0.2

--- app.pl
+++ app.pl
@@ -1,12 +1,17 @@
 #!/usr/bin/env perl
+use strict;
+use warnings;
 use Mojolicious::Lite;
 use Sys::Hostname;
 
+use constant VERSION => 0.2;
 
 get '/' => sub {
   my $conn = shift;
   my $host = hostname;
   $conn->stash(hostname => $host);
+  $conn->stash(version => VERSION);
+
   $conn->render(template => 'index');
 };
 
@@ -17,7 +22,8 @@ __DATA__
 @@ index.html.ep
 % layout 'default';
 % title 'DNAAS /dev/null as a Service';
-<h1>Hostname <%= $hostname %> </h1>
+<h1>Hostname: <%= $hostname %> </h1>
+<h1>App version: <%= $version %> </h1>
 
 @@ layouts/default.html.ep
 <!DOCTYPE html>
