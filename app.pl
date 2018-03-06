0015c92c9e8fe81db1eee7c4195d973d8f223263
Matthew Gallagher <mattva01@gmail.com>
2018-03-06 17:49:59 -0500

    Version 0.3

--- app.pl
+++ app.pl
@@ -4,14 +4,17 @@ use warnings;
 use Mojolicious::Lite;
 use Sys::Hostname;
 
-use constant VERSION => 0.2;
+use constant VERSION => 0.3;
 
 get '/' => sub {
   my $conn = shift;
   my $host = hostname;
+  if ($ENV{KUBE_NODE_NAME}){
+      $conn->stash(kube_node_name => $ENV{KUBE_NODE_NAME});
+  }
   $conn->stash(hostname => $host);
   $conn->stash(version => VERSION);
-
+  
   $conn->render(template => 'index');
 };
 
@@ -24,7 +27,9 @@ __DATA__
 % title 'DNAAS /dev/null as a Service';
 <h1>Hostname: <%= $hostname %> </h1>
 <h1>App version: <%= $version %> </h1>
-
+<% if (my $node_name = stash 'kube_node_name') {%>                                  
+    <h1>Kubernetes Node: <%= $node_name %>  </h1>
+<%}%>
 @@ layouts/default.html.ep
 <!DOCTYPE html>
 <html>
